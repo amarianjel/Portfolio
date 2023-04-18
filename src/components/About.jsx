9850ec624cc082859bc20af528082f6dbb5048a9
amarianjel <abraham.marianjel@gmail.com>
2023-04-17 21:17:25 -0400

    Modificando el logo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,22 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
+import imgHTML from '../assets/img/skills/html.png'
+import imgCSS from '../assets/img/skills/css3.png'
+import imgJAVASCRIPT from '../assets/img/skills/javascript.png'
 import imgBoostrap from '../assets/img/skills/bootstrap-logo-shadow.png'
+import imgNodejs from '../assets/img/skills/node.png'
+import imgReact from '../assets/img/skills/react.png'
+import imgSql from '../assets/img/skills/sql.png'
+import imgGit from '../assets/img/skills/git.png'
+import imgTrello from '../assets/img/skills/trello.png'
+import imgPhp from '../assets/img/skills/php.png'
+import imgPython from '../assets/img/skills/python.png'
+import imgJava from '../assets/img/skills/java.png'
+import imgMachineLearning from '../assets/img/skills/jupyter.png'
+import imgAngular from '../assets/img/skills/angular.png'
+import imgYii2 from '../assets/img/skills/yii.png'
+import imgMaterialize from '../assets/img/skills/materialize.png'
+import imgBPMN from '../assets/img/skills/bizagi.png'
 
 export const About = () => {
     const [hover, setHover] = useState(false);
@@ -33,26 +49,109 @@ export const About = () => {
             <div className="col-2">
                 <div className="about-item skills">
                     <h1>Habilidades</h1>
-                    <span className="skill">HTML</span>
-                    <span className="skill">CSS</span>
-                    <span className="skill">JavaScript</span>
-
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgHTML } alt="Imagen Habilidad" /> : 'HTML'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgCSS } alt="Imagen Habilidad" /> : 'CSS'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgJAVASCRIPT } alt="Imagen Habilidad" /> : 'JavaScript'}
+                    </span>
                     <span 
                         className={hover ? 'imgSkill' : 'skill'}
                         onMouseEnter={handleMouseEnter}
                         onMouseLeave={handleMouseLeave}>
                         {hover ? <img src={ imgBoostrap } alt="Imagen Habilidad" /> : 'Boostrap'}
                     </span>
-
-                    <span className="skill">Node.js</span>
-                    <span className="skill">React</span>
-                    <span className="skill">SQL</span>
-                    <span className="skill">Git</span>
-                    <span className="skill">API</span>
-                    <span className="skill">Unix/Linux</span>
-                    <span className="skill">Jira</span>
-                    <span className="skill">Confluence</span>
-                    <span className="skill">Figma</span>
+                    {/*  */}
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgNodejs } alt="Imagen Habilidad" /> : 'NodeJs'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgReact } alt="Imagen Habilidad" /> : 'React'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgSql } alt="Imagen Habilidad" /> : 'SQL'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgGit } alt="Imagen Habilidad" /> : 'Git'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgTrello } alt="Imagen Habilidad" /> : 'Trello'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgPhp } alt="Imagen Habilidad" /> : 'PHP'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgPython } alt="Imagen Habilidad" /> : 'Python'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgJava } alt="Imagen Habilidad" /> : 'Java'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgMachineLearning } alt="Imagen Habilidad" /> : 'Machine Learning'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgAngular } alt="Imagen Habilidad" /> : 'Angular'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgYii2 } alt="Imagen Habilidad" /> : 'Yii 2'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgMaterialize } alt="Imagen Habilidad" /> : 'Materialize CSS'}
+                    </span>
+                    <span 
+                        className={hover ? 'imgSkill' : 'skill'}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>
+                        {hover ? <img src={ imgBPMN } alt="Imagen Habilidad" /> : 'BPMN'}
+                    </span>
                 </div>
 
                 <div className="about-item languages">
