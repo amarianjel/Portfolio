a435499e1539dc467e4a9ba5c827ee886fe1cf7d
amarianjel <abraham.marianjel@gmail.com>
2023-04-19 00:32:15 -0400

    Contacto listo

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,49 +10,46 @@ export const Experience = () => {
                         <div className="timeline-date">2023 - ahora</div>
                         <div className="timeline-content">
                             <h3>Líder de Proyecto</h3>
-                            <p>
-                                Fondos estatales adjudicados para el club Trawen
-                                <p>FONDEPORTE y GORE</p>
-                            </p>
+                            <p>Fondos estatales adjudicados para el club Trawen</p>
+                            <p>FONDEPORTE y GORE</p>
                         </div>
                     </div>
                         <div className="timeline-date">2022</div>
                         <div className="timeline-content">
                             <h3>Ayudante de Asignatura</h3>
-                            <p>
-                            Modelamiento de la Información / Base de Datos
-                                <p>Universidad del Bio Bio</p>
-                            </p>
+                            <p>Modelamiento de la Información / Base de Datos</p>
                         </div>
                     </div>
                     <div className="timeline-item">
                         <div className="timeline-date">2022</div>
                         <div className="timeline-content">
                             <h3>Freelancer</h3>
-                            <p>
-                                Líder de Proyecto
-                                <p>Aplicación Tumuñan</p>
-                            </p>
+                            <p>Líder de Proyecto</p>
+                            <p>Aplicación Tumuñan</p>
+                        </div>
+                    </div>
+                    <div className="timeline-item">
+                        <div className="timeline-date">2022</div>
+                        <div className="timeline-content">
+                            <h3>Práctica Profesional</h3>
+                            <p>Prácticante</p>
+                            <p>Extraccion de datos</p>
                         </div>
                     </div>
                     <div className="timeline-item">
                         <div className="timeline-date">2021</div>
                         <div className="timeline-content">
                             <h3>Proyectos concursables</h3>
-                            <p>
-                                Proyectos Universitarios ganados
-                                <p>FADE</p>
-                            </p>
+                            <p>Proyectos Universitarios ganados</p>
+                            <p>FADE</p>
                         </div>
                     </div>
                     <div className="timeline-item">
                         <div className="timeline-date">2019</div>
                         <div className="timeline-content">
                             <h3>Soporte Técnico</h3>
-                            <p>
-                                Departamento de Servicios Tecnológicos 
-                                <p>Universidad del Bio Bio</p>
-                            </p>
+                            <p> Departamento de Servicios Tecnológicos</p>
+                            <p>Universidad del Bio Bio</p>
                         </div>
                     </div>
                 </div>
